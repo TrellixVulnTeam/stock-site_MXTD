6a434aa44e2aefcf1878392fcb5faf690ec90f60
yukiya-sakamoto <sakamoto@guruguru.info>
2021-02-02 13:46:15 +0900

    formがうまく動かないーー

--- blog/views.py
+++ blog/views.py
@@ -17,9 +17,11 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
+            post.photo = request.FILES['photo']
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+
